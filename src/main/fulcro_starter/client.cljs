2a769604bfb3bca9140839b1762a7727cae8f1d3
Niluka Satharasinghe (folcon) <folcon@gmail.com>
2020-05-12 18:45:48 +0100

    Basic tutorial commit

---
+++ src/main/fulcro_starter/client.cljs
@@ -0,0 +1,26 @@
+(ns fulcro-starter.client
+  (:require
+   [com.fulcrologic.fulcro.application :as app]
+   [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
+   [com.fulcrologic.fulcro.dom :as dom]))
+
+(defonce app (app/fulcro-app))
+
+(defsc Thing [this props]
+  {}
+  (dom/div
+    (dom/p "TEST")
+    (dom/p props)))
+
+(defn ^:export init
+  "Shadow-cljs sets this up to be our entry-point function. See shadow-cljs.edn `:init-fn` in the modules of the main build."
+  []
+  (app/mount! app Thing "app")
+  (js/console.log "Loaded"))
+
+(defn ^:export refresh
+  "During development, shadow-cljs will call this on every hot reload of source. See shadow-cljs.edn"
+  []
+  ;; re-mounting will cause forced UI refresh, update internals, etc.
+  (app/mount! app Thing "app")
+  (js/console.log "Hot reload"))
